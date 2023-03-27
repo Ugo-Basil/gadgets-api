a85c298cdbc4c4b044c54f13dd2385a093b61d4d
Basil <ohuruogubasil@gmail.com>
2023-03-27 14:21:19 +0100

    setup

---
+++ gadgets/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import config from 'config';
+@Module({
+  imports: [
+    MongooseModule.forRoot(config.get('mongodbUrl'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      w: 1,
+      keepAlive: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
